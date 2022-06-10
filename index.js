4f843286d7710b854ab6f54c87484c6f1388faab
Isidro Emir <88697022+IsidroEmirPelli@users.noreply.github.com>
2022-06-09 23:16:29 -0300

    fixing no value js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
 const btn = document.getElementById('btn');
 const cant = document.getElementById("cant");
 const result = document.getElementById("resultado")
 function get_dolar() {
-    if (cant!=''){
+    if (cant.value) {
         const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+        console.log(cant.value)
         fetch(url)
             .then(function (response) {
                 return response.json();})
@@ -14,5 +14,8 @@ function get_dolar() {
                 result.innerHTML = "El resultado es: $"+resultado+" ARS";
                 result.classList.remove("d-none")
             })
-        }
     }
+    else{
+        alert("Ingrese una cantidad")
+    }
+}
