e724bcf12dc8df6fb7a0152824cfd10627e528b6
Isidro Emir <88697022+IsidroEmirPelli@users.noreply.github.com>
2022-06-09 23:02:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const btn = document.getElementById('btn');
 const cant = document.getElementById("cant");
 const result = document.getElementById("resultado")
 function get_dolar() {
-    const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
-    fetch(url)
-        .then(function (response) {
-            return response.json();})
-        .then( function (data) {
-            const dolar = data[1].casa.venta
-            const resultado =parseInt(dolar)*parseInt(cant.value);
-            result.innerHTML = "El resultado es: $"+resultado+" ARS";
-            result.classList.remove("d-none")
-        })
+    if (cant!=''){
+        const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+        fetch(url)
+            .then(function (response) {
+                return response.json();})
+            .then( function (data) {
+                const dolar = data[1].casa.venta
+                const resultado =parseInt(dolar)*parseInt(cant.value);
+                result.innerHTML = "El resultado es: $"+resultado+" ARS";
+                result.classList.remove("d-none")
+            })
+        }
     }
